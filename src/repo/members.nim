c2d3346f711c635614b012b36cc6643547432fba
Sergey Bochkov <bochkovsa@gmail.com>
2019-01-19 21:48:43 +0500

    fix sql

--- src/repo/members.nim
+++ src/repo/members.nim
@@ -12,7 +12,7 @@ proc newMembers*(db : DbConn) : Members =
 proc all*(members: Members) : seq[Member] =
     var
         retre : seq[Member] = @[]
-        rows : seq[Row] = members.db.getAllRows(sql("SELECT * FROM members"))
+        rows : seq[Row] = members.db.getAllRows(sql("SELECT * FROM members ORDER BY weight"))
     for row in rows:
         retre.add(
             newMember(row[0].parseInt(), row[1], row[2])
