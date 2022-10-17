9bce5d42064ca7aa5254212b24d7856f9bccb42b
Sergey Bochkov <bochkovsa@gmail.com>
2022-10-17 10:35:00 +0500

    syntax fix

--- src/service/gigs.nim
+++ src/service/gigs.nim
@@ -4,7 +4,7 @@ import "../model/gig"
 import "../repo/gig_repo"
 import "services"
 
-let DT_START: DateTime = "1982-10-16".parse("yyyy-MM-dd")
+let dtStart: DateTime = "1982-10-16".parse("yyyy-MM-dd")
 
 type
     Gigs* = ref object of Service
@@ -13,5 +13,5 @@ type
 proc newGigs*(gigRepo: GigRepo) : Gigs =
     return Gigs(gigRepo: gigRepo)
 
-proc findAll*(this: Gigs, since: DateTime = DT_START): seq[Gig] =
+proc findAll*(this: Gigs, since: DateTime = dtStart): seq[Gig] =
     return this.gigRepo.all(since)
